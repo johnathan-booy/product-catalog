6428af54faf1718140dceccb53ea2c551dd30695
Johnathan Booy <106690990+johnathan-booy@users.noreply.github.com>
2025-08-24 20:51:34 -0700

    feat: Setup initial frontend Vue application

---
+++ fe/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <h1>Welcome</h1>
+    <p>Dynamic Product Catalog</p>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+#app {
+  text-align: center;
+  padding: 60px;
+  font-family: Arial, sans-serif;
+}
+</style>
